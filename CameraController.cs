4df7b535a91814b90d2e14b4212e9154c3a839c4
leonardo.nascimento <leonardo8544@gmail.com>
2022-05-04 14:19:26 -0300

    primeiro commit

---
+++ CameraController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject playerGameObject;
+
+    Vector2 cameraRotationValue = Vector2.zero;
+    Transform playerTransform;
+
+    public Vector3 cameraOffset;
+    public float mouseSensitivity;
+
+    void Start()
+    {
+        playerTransform = playerGameObject.transform;
+    }
+
+    void Update()
+    {
+        GetInput();
+        CameraRotation();
+    }
+
+    void LateUpdate()
+    {
+        LookAtCharacter();
+    }
+
+    void GetInput()
+    {
+        cameraRotationValue.x += Input.GetAxis("Mouse X") * mouseSensitivity;
+        cameraRotationValue.y += Input.GetAxis("Mouse Y") * mouseSensitivity;
+    }
+
+    void LookAtCharacter()
+    {
+        transform.LookAt(playerTransform);
+    }
+
+    void CameraRotation()
+    {
+        Quaternion newRot = Quaternion.Euler(-cameraRotationValue.y, cameraRotationValue.x, 0f);
+
+        playerTransform.rotation = SmoothDamp.Rotate(playerTransform.rotation, newRot, .3f, .5f);
+    }
+}
