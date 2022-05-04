302716974bdbd4864103534d25a5bcb5292932c8
leonardo.nascimento <leonardo8544@gmail.com>
2022-05-04 14:24:50 -0300

    added folders

---
+++ PhysicsBasedBallMovement/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    Rigidbody rb;
+
+    [SerializeField]
+    float inputStrenght;
+
+    Vector2 movementInputs = Vector2.zero;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        GetInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 forwardDir = player.transform.forward.normalized * movementInputs.y;
+        Vector3 rightDir = player.transform.right.normalized * movementInputs.x;   
+
+        rb.AddForce(forwardDir, ForceMode.Force);
+        rb.AddForce(rightDir, ForceMode.Force);
+    }
+
+    void GetInputs()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        movementInputs = new Vector2(inputX * inputStrenght, inputY * inputStrenght);
+    }
+}
